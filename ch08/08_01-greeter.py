561b72298a6c20c56375b3ab9eba8d7d991c5af8
remotephone@gmail.com <remotephone@gmail.com>
2018-03-28 22:15:29 -0500

    starting functions

---
+++ ch08/08_01-greeter.py
@@ -0,0 +1,8 @@
+# Define the function. Information in the () is passed to the function
+def greet_user(username):
+    # Docstring - used for documentation of functions
+    """Display a simple greeting."""
+    print("Hello, " + username.title() + "!")
+
+# Call the function
+greet_user('jesse')
---
+++ ch08/08_02-tryityourself1.py
@@ -0,0 +1,9 @@
+def display_message():
+    print("I am learning to write functions.")
+
+
+def favorite_book(title):
+    print("My favorite book is " + title.title() + ".")
+
+display_message()
+favorite_book('dune')
