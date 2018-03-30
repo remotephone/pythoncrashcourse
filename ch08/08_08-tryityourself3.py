dff89856340a7a0fc70206c6cb3e9b0bc283c18b
unknown <remotephone@gmail.com>
2018-03-29 23:12:30 -0500

    and some comments

--- ch08/08_08-tryityourself3.py
+++ ch08/08_08-tryityourself3.py
@@ -3,8 +3,9 @@
 
 def city_country(city, country):
     return city.title() + ", " + country.title()
-
+# pretty simple but just did this 2 different ways. print simply.
 print(city_country('havana', 'cuba'))
+# define two and then print them.
 pair2 = city_country('milan', 'italy')
 pair3 = city_country('paris', 'france')
 print(pair2)
@@ -18,6 +19,7 @@ def make_album(artist, title):
     album = {'musician': artist, 'record': title}
     return album
 
+# give the user 3 changes to define an album. This is clunky.
 singer = input("Who made this first recording? ")
 albumname = input("What is it called?")
 
@@ -27,20 +29,25 @@ albumname2 = input("What is it called?")
 singer3 = input("Who made this third recording? ")
 albumname3 = input("What is it called?")
 
+# set the user input to different variables made from the function
 thehits1 = make_album(singer, albumname)
 thehits2 = make_album(singer2, albumname2)
 thehits3 = make_album(singer3, albumname3)
 
+# print the resulting data. 
 print(thehits1)
 print(thehits2)
 print(thehits3)
 """
+# create you function that makes a dictionary.
 def make_album(artist, title):
     album = {'musician': artist, 'record': title}
     return album
 
+# start a while loop.
 while True:
     print("(enter 'q' at any time to quit)")
+    # user input goes to singer and albumname. Not using make_album() yet.
     singer = input("What is the artists name? ")
     if singer == 'q':
         break
@@ -48,7 +55,9 @@ while True:
     if albumname == 'q':
         break
     
+    # define record as the result of make_album(). Now we're using the function.
     record = make_album(singer, albumname)
+    # Call the values ("artists") from the keys ("musician") from record. 
     print("The album " + record['musician'].title() + " was made by "\
           + record['record'].title() + ".")
     
