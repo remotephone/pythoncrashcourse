90b95645496dadc475a50e5c90e96bd89d843e99
unknown <remotephone@gmail.com>
2018-04-04 23:58:02 -0500

    syncing

---
+++ ch01-05/02-2_tryityourself.py
@@ -0,0 +1,28 @@
+# Personal message
+name = "Tim Timson"
+print("Hello, " + name + ", would you like to learn some Python today?")
+print("========\n")
+
+# Name Cases
+namecase = "john johnson"
+print("Print the name lower, upper, and title case")
+print(namecase.lower())
+print(namecase.upper())
+print(namecase.title())
+print("========\n")
+
+# A little quote
+print('Alejandro once said, "You\'re asking me how a watch works. For now, just keep an eye on the time."')
+
+print("========\n")
+
+famous_person = "Alejandro"
+print(famous_person + ' once said, "You\'re asking me how a watch works. For now, just keep an eye on the time."')
+
+
+print("========\n")
+
+name_space = " Pablo Pablodez "
+print(name_space.rstrip())
+print(name_space.lstrip())
+print(name_space.strip())
